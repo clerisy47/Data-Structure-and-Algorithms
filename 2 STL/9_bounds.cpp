e6e2d42be33b7d96368279f5659d37373662538f
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-21 22:24:14 +0545

    add new leetcode problem

--- 2 STL/9_bounds.cpp
+++ 2 STL/9_bounds.cpp
@@ -1,3 +1,10 @@
+// search: smallest index such that arr[i]==k (target)
+// lower_bound: smallest index such that arr[i]>=k (target)
+// upper_bound: smallest index such that arr[i]>k (target)
+// if all elements are smaller than target it returns length of array ie hypothetical index (n)
+// if multiple elements are present then it returns first index
+// for sorted array O(logn) for unsorted array O(n)
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -12,24 +19,20 @@ int main(){
 
     vector<int> v = {4, 6, 8, 25, 7};
 
-    // lower_bound always returns the number if present or number just larger to it
-    // upper_bound always returns the number just greater
-    // for sorted array O(logn) for unsorted array O(n)
-
     auto bound = lower_bound(v.begin(), v.end(), 6);
-    cout<< *bound<<endl; // 6
+    cout<< bound-v.begin()<<endl; // 1
 
     bound = upper_bound(v.begin(), v.end(), 6);
-    cout<< *bound<<endl; // 8
+    cout<< bound-v.begin()<<endl; // 2
 
     bound = upper_bound(v.begin(), v.end(), 26);
-    cout<< (bound==v.end())<<endl; // 1 ie not found nor greater element
+    cout<< bound-v.begin()<<endl; // 5 (if element isn't present it returns the length of array ie bound==v.end())
+    cout<< *bound<<endl; // 0
+
 
     // For sets and maps, use s.lower_bound() instead of lower_bound() as it is optimized to log(n)
     set<int> s = {4, 6, 8, 25, 7};
     auto bound_set = s.lower_bound(6);
-    cout<< *bound_set<<endl; // 6
-
-
+    cout<< *bound_set<<endl; // 6 (we can't acess index of set hence returning the element itself)
 
 }
