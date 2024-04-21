5ebbb31837e3fc942cdb32fa6c80a78b98594968
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-21 22:25:01 +0545

    add new leetcode problem

---
+++ 5 Arrays and Hashings/5 Binary Search/1 Basics/4_first_and_last.cpp
@@ -0,0 +1,58 @@
+// Find the first and last occurrence of ‘k’ in 'arr'.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// using lower bound and upper bound
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    int lbound = lower_bound(arr.begin(), arr.end(), k) - arr.begin();
+    if (arr[lbound] != k || lbound == n)
+        return {-1, -1};
+    int ubound = upper_bound(arr.begin(), arr.end(), k) - arr.begin();
+    return {lbound, ubound - 1};
+}
+// TC : 2logn
+
+// using binary search
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    int left = 0, right = n - 1, first = -1, last = -1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == k)
+        {
+            first = mid;
+            right = mid - 1; // going left to get smallest index
+        }
+        else if (arr[mid] > k)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    if (first == -1)
+        return {-1, -1};
+    left = 0, right = n - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == k)
+        {
+            last = mid;
+            left = mid + 1; // going right to get largest index
+        }
+        else if (arr[mid] > k)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return {first, last};
+}
+// TC : 2logn
+
+int main()
+{
+
+    return 0;
+}
