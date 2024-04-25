25b16a571a9165aa2680766c84cecac3dce2a29d
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-25 10:32:29 +0545

    add leetcode problem

---
+++ 8 Stacks and Queues/3 Monotonic Stack:Queue/2_next_greater_element_1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute Force: Run two for loops for next greater element and push it to array
+
+// Optimal
+vector<int> nextGreaterElement(vector<int> &arr, int n)
+{
+    stack<int> st;
+    vector<int> output(n);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && st.top() <= arr[i])
+        {
+            st.pop();
+        }
+        if (st.empty())
+        {
+            output[i] = -1;
+        }
+        else
+        {
+            output[i] = st.top();
+        }
+        st.push(arr[i]);
+
+    }
+    return output;
+}
+// Time Complexity: O(2n)
+
+int main()
+{
+
+    return 0;
+}
