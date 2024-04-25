c9d502e90e0ed90cc201e66cb7961342545c43eb
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-25 10:32:27 +0545

    add leetcode problem

---
+++ 8 Stacks and Queues/3 Monotonic Stack:Queue/1_balanced_paranthesis.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isBalanced(string s){
+    stack<char> st;
+
+    for(char c: s){
+        if(c=='['||c=='('||c=='{'){
+            st.push(c);
+        }
+        else {
+            if(st.empty()) return false; 
+            if(c==']' && st.top()=='[')
+                st.pop();
+            else if(c==')' && st.top()=='(')
+                st.pop();
+            else if(c=='}' && st.top()=='{')
+                st.pop();
+            else
+                return false;
+        }
+    }
+    return st.empty();
+}
+
+int main(){
+    
+    return 0;
+}
