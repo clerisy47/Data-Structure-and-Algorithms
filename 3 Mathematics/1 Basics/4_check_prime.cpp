980e61e175c2566951f3ba7441904fd798af3411
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-26 21:54:46 +0545

    add hacker earth problem

---
+++ 3 Mathematics/1 Basics/4_check_prime.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check_prime(int n)
+{
+    int sum = 0;
+    if (n < 2)
+        return 0;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+    // Time Complexity: O(n)
+
+    // For twin prime check if their difference is 2 and both are prime numbers.
