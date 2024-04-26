297cf6b91acaf64c4d4a2e7efea716c54f236b4e
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-26 21:57:38 +0545

    Add hackerearth problem

---
+++ 4 Recursion, Backtracking and DP/3 Subsequences Pattern/3_subsequence_string.cpp
@@ -0,0 +1,35 @@
+// similar but little bit more optimized way (no recursive stack space) in bit manipulation folder
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void generate(int i, string &str_a, string &str_q, vector<string> &arr)
+{
+    if (i == str_q.size())
+    {
+        arr.push_back(str_a);
+        return;
+    }
+    // generates non empty strings
+    str_a.push_back(str_q[i]);
+    generate(i + 1, str_a, str_q, arr);
+    str_a.pop_back();
+
+    // generates empty strings
+    generate(i + 1, str_a, str_q, arr);
+
+}
+
+vector<string> generateSubsequences(string str_q)
+{
+    vector<string> arr;
+    string str_a;
+    generate(0, str_a, str_q, arr);
+    return arr;
+}
+
+int main()
+{
+
+    return 0;
+}
