8aa0de137826f52c49098dc013be6de733547ada
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-26 21:56:09 +0545

    Add hackerearth problem

---
+++ 3 Mathematics/2 Advanced/5_modular_arithmetic.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// TODO Debug and complete divide problem
+
+int mod_sum(int x, int y, int m)
+{
+    return (x % m + y % m) % m;
+}
+
+int mod_sub(int x, int y, int m)
+{
+    return (x % m - y % m + m) % m;
+}
+
+int mod_multiply(int x, int y, int m)
+{
+    return (x % m * y % m)%m;
+}
+
+int mod_power(int x, int y, int m){
+    int ans=1;
+    while(y){
+        if(y%2==0){
+            x = (x*x)%m;
+            y= y/2;
+        }
+        else{
+            ans= (ans*x)%m;
+            y= y-1;
+        }
+    }
+    return ans;
+}
+
+int mod_divide(int x, int y, int m){
+    int mmi = 1;
+    return (x % m * mmi % m)%m;
+
+}
+
+int mod_factorial(int x, int m){
+    int ans=1;
+    for(int i=2; i<=x; i++){
+        ans= (ans*2)%m;
+    }   
+}
+
+int main()
+{
+
+    return 0;
+}
