283b80b878b932f23bdc2af927fc23cad387bd1a
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-28 09:01:37 +0545

    optimised binary search

--- 5 Arrays and Hashings/5 Binary Search/1 Basics/3_floor_ceil.cpp
+++ 5 Arrays and Hashings/5 Binary Search/1 Basics/3_floor_ceil.cpp
@@ -4,30 +4,33 @@ using namespace std;
 // brute force: linear search
 
 // optimal:
-pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int k) {
-	int left=0, right=n-1, answer_f=-1, answer_c=-1; 
-    // ceil
-    while(left<=right){                     // ceil is basically element in lower bound
-        int mid = left + (right-left)/2;   
+
+int eleLowerBound(vector<int> arr, int n, int k) {
+    int left=0, right=n-1;
+    while(left<=right){
+        int mid = left + (right-left)/2;
         if(arr[mid]>=k){
-            answer_c=arr[mid];
+            if(mid==0||arr[mid-1]<k) return arr[mid];
 			right = mid-1;
         }
         else left = mid+1;
     }
-    // floor
-	left=0, right=n-1;
+    return -1;  // if all elements are smaller than target it returns -1
+}
+
+pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int k) {
+	int left=0, right=n-1, answer=-1; 
 	while(left<=right){
         int mid = left + (right-left)/2; 
-        if(arr[mid]<=k){               
-            answer_f=arr[mid];
+        if(arr[mid]<=k){    
+            if(mid==n-1||arr[mid+1]>k) return {arr[mid], eleLowerBound(arr, n, k)};      
 			 left = mid+1;
         }
         else right = mid-1;
-
     }
-    return {answer_f, answer_c};
+    return {-1, eleLowerBound(arr, n, k)}; // ceil is basically element in lower bound
 }
+
 int main(){
     
     return 0;
