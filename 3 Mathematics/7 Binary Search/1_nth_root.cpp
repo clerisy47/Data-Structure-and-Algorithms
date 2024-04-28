22bd8b495b575325f24644070ad3806c69983218
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-28 09:01:07 +0545

    add leetcode problems

---
+++ 3 Mathematics/7 Binary Search/1_nth_root.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Brute force: Running loop from 1 to n to check if there is nth root
+
+// Optimal
+long long pow(int x, int y, int limit)
+{ // power function which doesn't let ans cross the limit
+    long long ans = 1;
+    for (int i = 0; i <= y - 1; i++)
+    {
+        ans *= x;
+        if (ans > limit ||ans > INT_MAX / x)
+        {
+            return limit + 1;
+        }
+    }
+    return ans;
+
+}
+
+int NthRoot(int n, int num)
+{
+    int left = 1, right = num;
+    int ans = num;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (pow(mid, n, num) <= num)
+        {
+            ans = mid;
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
