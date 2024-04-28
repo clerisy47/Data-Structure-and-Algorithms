f32226639f2f4c5ba3edcbcf9eeb9de3ceadcfe1
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-28 09:01:34 +0545

    optimised binary search

--- 5 Arrays and Hashings/5 Binary Search/1 Basics/2_lower_upper_bound.cpp
+++ 5 Arrays and Hashings/5 Binary Search/1 Basics/2_lower_upper_bound.cpp
@@ -16,29 +16,29 @@ using namespace std;
 
 // Binary Search
 int lowerBound(vector<int> arr, int n, int k) {
-    int left=0, right=n-1, answer=n; // if all elements are smaller than target it returns length of array ie hypothetical index (n)
+    int left=0, right=n-1;
     while(left<=right){
         int mid = left + (right-left)/2;
         if(arr[mid]>=k){
-            answer=mid;
+            if(mid==0||arr[mid-1]<k) return mid;
 			right = mid-1;
         }
         else left = mid+1;
     }
-    return answer; 
+    return n;  // if all elements are smaller than target it returns length of array ie hypothetical index (n)
 }
 
 int upperBound(vector<int> &arr, int k, int n){
-    int left=0, right=n-1, answer=n; // if all elements are smaller than target it returns length of array ie hypothetical index (n)
+    int left=0, right=n-1;
     while(left<=right){
         int mid = left + (right-left)/2;
-        if(arr[mid]>k){ // just modify this
-            answer=mid;
+        if(arr[mid]>k){
+            if(mid==0||arr[mid-1]<=k) return mid;
 			right = mid-1;
         }
         else left = mid+1;
     }
-    return answer; 	
+    return n; 	// if all elements are smaller than target it returns length of array ie hypothetical index (n)
 }
 
 int main(){
