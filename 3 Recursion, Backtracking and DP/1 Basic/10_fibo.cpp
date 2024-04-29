ca6bdd5c0be3a2ebe9f943ac327466e4b488f202
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-29 22:38:38 +0545

    add leetcode problem

--- 3 Recursion, Backtracking and DP/1 Basic/10_fibo.cpp
+++ 3 Recursion, Backtracking and DP/1 Basic/10_fibo.cpp
@@ -1,13 +1,12 @@
 // 0 1 1 2 3 5 8 13
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int DP[1000];
 // Basic Recursion
 int fibonacci(int n)
 {
-    if (n == 0 || n == 1)
+    if (n <= 1)
         return n;
     return (fibonacci(n - 1) + fibonacci(n - 2));
 }
@@ -15,36 +14,55 @@ int fibonacci(int n)
 // Time Complexity O(2^n)
 // Space Complexity O(n)
 
-
 // Memoization
-int fibonacci(int n)
+int fibonacci(int n, int memoization[]) // size of memoization = x+1
 {
-    if (n == 0 || n == 1)
+    if (n <= 1)
         return n;
-    if(DP[n]!=-1)
-    return DP[n];
-
-    return DP[n] = (fibonacci(n - 1) + fibonacci(n - 2));
+    if (memoization[n])
+    {
+        return memoization[n];
+    }
+    return memoization[n] = (fibonacci(n - 1, memoization) + fibonacci(n - 2, memoization));
 }
 // Time Complexity O(n)
-// Space Complexity O(n)
+// Space Complexity O(2n)
 
 // Dynamic Programming
 int fibonacci(int n)
 {
-    DP[0]=0;
-    DP[1]=1;
-    for(int i=2; i<=n; i++){
-        DP[i]= DP[i-1]+ DP[i-2];
+    int dp[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
-    return DP[n];
+    return dp[n];
 }
 // Time Complexity O(n)
 // Space Complexity O(n)
 
+// Space Optimized Dynamic Programming
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    int dp[3];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[2] = dp[1] + dp[0];
+        dp[0] = dp[1];
+        dp[1] = dp[2];
+    }
+    return dp[2];
+}
+
 // O(logn) in mathematics folder
 
-int main(){
-    fill_n(DP, 1000, -1);
+int main()
+{
     return 0;
 }
