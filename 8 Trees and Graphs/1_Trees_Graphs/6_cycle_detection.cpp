3354498b8093ff757d79302b20608b62a6c3f5a1
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-30 14:45:57 +0545

    add leetcode problem

---
+++ 8 Trees and Graphs/1_Trees_Graphs/6_cycle_detection.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int vertex, int parent, vector<vector<int>> &graph, vector<bool> &visited, bool *isCycle)
+{
+    visited[vertex] = true;
+    for (auto child : graph[vertex])
+    {
+        if (visited[child] && child != parent)
+        {
+            *isCycle = true;
+            return;
+        }
+        if (!visited[child])
+        {
+            dfs(child, vertex, graph, visited, isCycle);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, v1, v2;
+    cin >> n >> m;
+    vector<vector<int>> graph(n+1);
+    vector<bool> visited(n+1);
+    bool isCycle = false;
+
+    for (int i = 1; i <= m; i++)
+    {
+        cin >> v1 >> v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (!visited[i])
+        {
+            dfs(i, 0, graph, visited, &isCycle);
+            if (isCycle)
+                break;
+        }
+    }
+    if (isCycle)
+        cout << "Cycle detected\n";
+    else
+        cout << "No cycle detected\n";
+    return 0;
+}
