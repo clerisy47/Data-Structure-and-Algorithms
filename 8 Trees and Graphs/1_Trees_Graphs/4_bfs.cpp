834b0808ab5fc9f5a9deab776be35024bcca23a6
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-04-30 14:45:47 +0545

    add leetcode problem

---
+++ 8 Trees and Graphs/1_Trees_Graphs/4_bfs.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bfs(int vertex, vector<vector<int>> &graph)
+{
+    queue<int> q;
+    vector<bool> visited(graph.size(), false);
+    q.push(vertex);
+    visited[vertex] = true;
+    vector<int> output;
+    while (!q.empty())
+    {
+        for (int child : graph[q.front()])
+        {
+            if (!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+            }
+        }
+        output.push_back(q.front());
+        q.pop();
+    }
+    return output;
+}
+// TC: O(V+ 2E) for single directional O(V+E)
+// SC : O(2V) (for visited and queue) + O(V) (for output)
+
+int main()
+{
+    return 0;
+}
