1d7e1ee65fe9a04f994bc74b9ee2cf1e21894cf1
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-04 20:28:03 +0545

    Remove bugs

--- 5 Strings/1 Basics/1_capital.cpp
+++ 5 Strings/1 Basics/1_capital.cpp
@@ -3,14 +3,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// basic ascii arithmetic
+// string toUpper(string &str)
+// {
+//     for (int i = 0; i <= str.size()-1; i++)
+//         {
+//             str[i]=str[i]+'A'-'a';
+//         }
+//     return str;
+// }
+
 string toUpper(string &str)
 {
     for (int i = 0; i <= str.size()-1; i++)
         {
-            str[i]=str[i]+'A'-'a';
+            str[i]=str[i]&(~(1<<5)); // unsetting fifth bit 
         }
     return str;
 }
+string toLower(string &str)
+{
+    for (int i = 0; i <= str.size()-1; i++)
+        {
+            str[i]=str[i]|(1<<5); // Setting fifth bit 
+        }
+    return str;
+}
+
+string toUpper(string &str)
+{
+    for (int i = 0; i <= str.size()-1; i++)
+        {
+            str[i]=str[i]&'_'; // is almost equivant to ~(1<<5)
+        }
+    return str;
+}
+string toLower(string &str)
+{
+    for (int i = 0; i <= str.size()-1; i++)
+        {
+            str[i]=str[i]| ' '; // ' ' is equivant to 1<<5 
+        }
+    return str;
+}
+
+
 
 int main()
 {
