20dbe1ab1f84a8390065e05a3ffb3c3156a669b7
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-05 21:20:56 +0545

    add new problem

---
+++ 2 Mathematics/2 Advanced/1_divisors.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// brute force
+void divisors(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            cout << i << endl;
+    }
+}
+
+// better
+void divisors(int n)
+{
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << endl;
+            if(i!=n/i){
+                cout<<n/i<<endl;
+            }
+        }
+    }
+}
+
+// optimized version in sieve folder
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    divisors(n);
+
+    return 0;
+}
