a0f2517f200dd4652af8ab58e35ebcafd63c11bb
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-07 22:11:42 +0545

    add new problem

--- 4 Arrays and Hashings/5 Binary Search/1 Basics/3_floor_ceil.cpp
+++ 4 Arrays and Hashings/5 Binary Search/1 Basics/3_floor_ceil.cpp
@@ -1,37 +1,37 @@
-#include<bits/stdc++.h>
+// floor and ceil are elements and not indexes
+// floor: largest element such that x<=k (target)
+// ceil: smallest element such that x]>=k (target)
+
+#include <bits/stdc++.h>
 using namespace std;
 
 // brute force: linear search
 
 // optimal:
 
-int eleLowerBound(vector<int> arr, int n, int k) {
-    int left=0, right=n-1;
-    while(left<=right){
-        int mid = left + (right-left)/2;
-        if(arr[mid]>=k){
-            if(mid==0||arr[mid-1]<k) return arr[mid];
-			right = mid-1;
+int getFloor(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    int left = 0, right = n - 1, answer = -1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] <= k)
+        {
+            if (mid == n - 1 || arr[mid + 1] > k)
+                return arr[mid];
+            left = mid + 1;
         }
-        else left = mid+1;
+        else
+            right = mid - 1;
     }
-    return -1;  // if all elements are smaller than target it returns -1
+    return -1;
 }
 
-pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int k) {
-	int left=0, right=n-1, answer=-1; 
-	while(left<=right){
-        int mid = left + (right-left)/2; 
-        if(arr[mid]<=k){    
-            if(mid==n-1||arr[mid+1]>k) return {arr[mid], eleLowerBound(arr, n, k)};      
-			 left = mid+1;
-        }
-        else right = mid-1;
-    }
-    return {-1, eleLowerBound(arr, n, k)}; // ceil is basically element in lower bound
-}
+// ceil is basically element in lower bound
 
-int main(){
-    
+int main()
+{
+
     return 0;
 }
