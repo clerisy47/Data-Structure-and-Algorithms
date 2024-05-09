6eab1cbc125c9255bccb223f7c410f4504207174
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-09 21:14:28 +0545

    add new problem

--- 3 Recursion, Backtracking and DP/1 Basic/10_fibo.cpp
+++ 3 Recursion, Backtracking and DP/1 Basic/10_fibo.cpp
@@ -8,58 +8,17 @@ int fibonacci(int n)
 {
     if (n <= 1)
         return n;
-    return (fibonacci(n - 1) + fibonacci(n - 2));
+    
+    int l =fibonacci(n-1);
+
+    int r = fibonacci(n-1);
+    return l+r;
 }
 
 // Time Complexity O(2^n)
 // Space Complexity O(n)
 
-// Memoization
-int fibonacci(int n, int memoization[]) // size of memoization = x+1
-{
-    if (n <= 1)
-        return n;
-    if (memoization[n])
-    {
-        return memoization[n];
-    }
-    return memoization[n] = (fibonacci(n - 1, memoization) + fibonacci(n - 2, memoization));
-}
-// Time Complexity O(n)
-// Space Complexity O(2n)
-
-// Dynamic Programming
-int fibonacci(int n)
-{
-    int dp[n + 1];
-    dp[0] = 0;
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    return dp[n];
-}
-// Time Complexity O(n)
-// Space Complexity O(n)
-
-// Space Optimized Dynamic Programming
-int fibonacci(int n)
-{
-    if (n <= 1)
-        return n;
-    int dp[3];
-    dp[0] = 0;
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        dp[2] = dp[1] + dp[0];
-        dp[0] = dp[1];
-        dp[1] = dp[2];
-    }
-    return dp[2];
-}
-
+// O(n) in dynamic programming folder
 // O(logn) in mathematics folder
 
 int main()
