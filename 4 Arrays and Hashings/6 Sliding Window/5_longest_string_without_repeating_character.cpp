3f1d352beae5f911e6864929d6d8d25130a3a7e8
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-10 22:29:22 +0545

    add new problem

---
+++ 4 Arrays and Hashings/6 Sliding Window/5_longest_string_without_repeating_character.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    int n = s.size(), length = 0, maxLength = 0;
+    map<char, int> hash;
+    for (int l = 0, r = 0; r <= n - 1; r++)
+    {
+        if (hash.find(s[r]) != hash.end()&&hash[s[r]]>=l) // &&hash[s[r]]>=l to ensure indexes before the window not to be considered
+        {
+            l = hash[s[r]] + 1;
+        }
+        hash[s[r]] = r; 
+        length = r - l + 1;  
+        maxLength = max(maxLength, length);
+    }
+    return maxLength;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
