14a0f40dbe0aee6d52c3ac471e48c42574e320f1
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-13 22:16:29 +0545

    add new problem

---
+++ 5 Trees and Graphs/5 Tries/2_Implementation_bits.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+
+    Node *child[2];
+
+    bool containsBit(int bit)
+    {
+        return (child[bit] != NULL);
+    }
+
+    Node *getChild(int bit)
+    {
+        return child[bit];
+    }
+
+    void putChild(int bit, Node *node)
+    {
+        child[bit] = node;
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+
+        root = new Node();
+    }
+
+    void insert(int num)
+    {
+        Node *node = root;
+        for (int i = 31; i >= 0; i--)
+        {
+            int bit = num & (1 << i); // get bit
+            if (!node->containsBit(bit))
+            {
+
+                node->putChild(bit, new Node());
+            }
+            node = node->getChild(bit);
+        }
+    }
+    int getMax(int num)
+    {
+        Node *node = root;
+        int maxNum = 0;
+        for (int i = 31; i >= 0; i--)
+        {
+            int bit = num & (1 << i); // get bit
+            if (node->containsBit(1 - bit))
+            {
+                maxNum |= (1 << i); // set bit
+
+                node = node->getChild(1 - bit);
+            }
+            else
+            {
+                node = node->getChild(bit);
+            }
+        }
+        return maxNum;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
