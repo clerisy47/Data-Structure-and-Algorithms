e16f877335fe1daa00cb17e2b42dab0f92960910
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-14 21:58:28 +0545

    Fix bug

--- 6 Recursion, Backtracking and DP/4 Dynamic Programming/2 2D 3D Grids/2_unique_paths_1_2.cpp
+++ 6 Recursion, Backtracking and DP/4 Dynamic Programming/2 2D 3D Grids/2_unique_paths_1_2.cpp
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/unique-paths/description/
 // https://leetcode.com/problems/unique-paths/description/
 
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -46,17 +45,18 @@ int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
         for (int j = 0; j <= n - 1; j++)
         {
             if (obstacleGrid[i][j])
+            {
                 dp[i][j] = 0;
-            else if (i == 0 && j == 0)
-            {
-                dp[0][0] = 1;
+                continue;
             }
-            else
+            if (i == 0 && j == 0)
             {
-                int up = j >= 1 ? dp[i][j - 1] : 0;
-                int left = i >= 1 ? dp[i - 1][j] : 0;
-                dp[i][j] = up + left;
+                dp[i][j] = 1; // writing dp[i][j] instead of dp[0][0] as we should replace i with 1 later and not with 0
+                continue;
             }
+            int up = j >= 1 ? dp[i][j - 1] : 0;
+            int left = i >= 1 ? dp[i - 1][j] : 0;
+            dp[i][j] = up + left;
         }
     }
     return dp[m - 1][n - 1];
@@ -64,7 +64,36 @@ int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
 // TC: O(N*M)
 // SC: O(N*M)
 
-// TODO: Space Optimization
+int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+{
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
+    vector<vector<int>> dp(2, vector<int>(n));
+    for (int i = 0; i <= m - 1; i++)
+    {
+        fill(dp[1].begin(), dp[1].end(), 0); // need to fill with zeroes as the base condition has 0 index related to it
+
+        for (int j = 0; j <= n - 1; j++)
+        {
+            if (obstacleGrid[i][j]) // for unique path 1 just remove this
+            {
+                dp[1][j] = 0;
+                continue;
+            }
+            if (i == 0 && j == 0)
+            {
+                dp[1][j] = 1;
+                continue;
+            }
+
+            int up = j >= 1 ? dp[1][j - 1] : 0;
+            int left = i >= 1 ? dp[0][j] : 0;
+            dp[1][j] = up + left;
+        }
+        dp[0] = dp[1];
+    }
+    return dp[0][n - 1];
+}
 
 int main()
 {
