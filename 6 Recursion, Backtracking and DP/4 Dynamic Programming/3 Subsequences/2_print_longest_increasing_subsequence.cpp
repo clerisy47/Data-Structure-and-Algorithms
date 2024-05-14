b9029384ecea3189b555524dcc8ed867ff0006a1
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-14 10:20:20 +0545

    add new problem

---
+++ 6 Recursion, Backtracking and DP/4 Dynamic Programming/3 Subsequences/2_print_longest_increasing_subsequence.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> longestIncreasingSubsequence(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> dp(n, 1), hash(n, -1); // if contains lis than it will contain the last index if not then i
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int prev = 0; prev <= i - 1; prev++)
+        {
+            if (arr[prev] < arr[i] && dp[prev] + 1 > dp[i])
+            {
+                dp[i] = dp[prev] + 1;
+                hash[i] = prev;
+            }
+        }
+    }
+    auto it = max_element(dp.begin(), dp.end());
+    int maxEle = *it, lasti = it - dp.begin();
+
+    vector<int> ans;
+    ans.push_back(arr[lasti]);
+
+    while (hash[lasti] != -1)
+    {
+        lasti = hash[lasti];
+        ans.push_back(arr[lasti]);
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
