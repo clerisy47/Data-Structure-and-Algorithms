8678aa112545051672c1eef22e367beeaff896f4
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-15 18:36:55 +0545

    fixed bugs

--- 3 Arrays, Strings and Hashing/5 Binary Search/2 Answers/3_koko_eating_banana.cpp
+++ 3 Arrays, Strings and Hashing/5 Binary Search/2 Answers/3_koko_eating_banana.cpp
@@ -3,9 +3,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int hoursTaken(vector<int> &piles, int eat_per_hour)
+long hoursTaken(vector<int> &piles, int eat_per_hour)
 {
-    int h_taken = 0;
+    long h_taken = 0;
     for (auto &x : piles)
     {
         h_taken += ceil(1.0 * x / eat_per_hour);
@@ -26,27 +26,26 @@ int minEatingSpeed(vector<int> &piles, int h)
 }
 
 // optimal
+
 int minEatingSpeed(vector<int> &piles, int h)
 {
     int maxEle = *max_element(piles.begin(), piles.end());
+    int ans = -1;
     for (int l = 1, r = maxEle; l <= r;)
     {
         int mid = l + (r - l) / 2;
-        int h_taken = hoursTaken(piles, mid);
-        if (h_taken == h)
+        long h_taken = hoursTaken(piles, mid); 
+        if (h_taken <= h)
         {
-            return mid;
-        }
-        else if (h_taken > h)
-        {
-            r = mid - 1;
+            ans = mid;
+            r = mid - 1;  // note that hoursTaken is inversely proportional to mid hence to increase h_taken, we should reduce mid
         }
         else
         {
             l = mid + 1;
         }
     }
-    return -1;
+    return ans;
 }
 
 int main()
