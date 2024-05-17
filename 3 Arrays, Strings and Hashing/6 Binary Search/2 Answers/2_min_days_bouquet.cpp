a658066804dcc6a9f67d6d5dfa9e17de5d0ad43d
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-17 10:03:17 +0545

    add new problem

---
+++ 3 Arrays, Strings and Hashing/6 Binary Search/2 Answers/2_min_days_bouquet.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/minimum-number-of-days-to-make-mid-bouquets/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nBouquets(vector<int> &bloomDay, int k, int day)
+{
+    int ctr = 0, n = bloomDay.size();
+    for (int i = 0, j = 0; j <= n - 1; j++)
+    {
+        if (bloomDay[j] > day)
+        {
+            i=j+1;
+        }
+        if (j - i + 1 == k)
+        {
+            ctr++;
+            i=j+1;
+
+        }
+    }
+    return ctr;
+}
+
+
+int minDays(vector<int> &bloomDay, int m, int k)
+{
+    int minele = *min_element(bloomDay.begin(), bloomDay.end());
+    int maxele = *max_element(bloomDay.begin(), bloomDay.end());
+    int n = bloomDay.size(), ans = -1;
+    for (int i = 0, j = n; i <= j;)
+    {
+        int mid = i + (j - i) / 2;
+        int number_of_bouquets = nBouquets(bloomDay, k, mid);
+        if (number_of_bouquets >= m)
+        {
+            ans = mid;
+            j = mid - 1;
+        }
+        else
+        {
+            i = mid + 1;
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
