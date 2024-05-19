60a918ffb7c6ecfa83b304df5df4f7e4bb81a91d
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-19 10:52:22 +0545

    add new problem

--- 3 Arrays, Strings and Hashing/7 Greedy Algorithms/17_stocks_buy_sell_2.cpp
+++ 3 Arrays, Strings and Hashing/7 Greedy Algorithms/17_stocks_buy_sell_2.cpp
@@ -3,15 +3,27 @@
 // solved the same problem using dp in dp folder
 // more variants in dp folder
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int maxProfit(vector<int> &prices)
+{
+    int max_profit = 0;
+    int n = prices.size();
+    for (int i = 0; i <= n - 2; i++)
+    {
+        if (prices[i] < prices[i + 1])
+        {
+            max_profit += prices[i + 1] - prices[i];
+        }
+    }
+    return max_profit;
+}
 
-
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     return 0;
 }
