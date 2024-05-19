dc96adcdd30376e1e2d5d2408f2db34f57a60844
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-19 09:06:05 +0545

    refractor

--- 6 Recursion, Backtracking and DP/4 Dynamic Programming/3 Subsequences/2_partition_with_min_difference.cpp
+++ 6 Recursion, Backtracking and DP/4 Dynamic Programming/3 Subsequences/2_partition_with_min_difference.cpp
@@ -3,6 +3,8 @@
 // |s1-s2| will be minimum if s1=s2=s/2 hence, we can check if sum exists from s/2 to 0
 // we don't need to call the isSubset function again and again as dp array will store if they exist from s/2 to 0
 
+// if there are negative elements in array as well, we should use hashmaps instead of nested vectors (as negative indexing isn't allowed)
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -31,6 +33,19 @@ vector<vector<bool>> isSubsetSum(vector<int> &arr, int k)
         }
     }
     return dp;
+}
+
+int minSubsetSumDifference(vector<int> &arr, int n)
+{
+    int sum = accumulate(arr.begin(), arr.end(), 0);
+    vector<vector<bool>> dp = isSubsetSum(arr, sum / 2);
+    for (int i = sum / 2; i >= 0; i--) // the lowest possible will be when its sum/2 and highest will be when it's 0 so we're traversing reversely
+    {
+        if (dp[n - 1][i])
+        {
+            return abs(2 * i - sum); // i - j = i - sum + i = 2*i - sum
+        }
+    }
 }
 
 int main()
