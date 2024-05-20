05abbc4dd54c460815468f3a2560e655f4762ec8
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-20 15:58:36 +0545

    optimized

--- 3 Arrays, Strings and Hashing/8 Recursion, Backtracking and DP/4 Dynamic Programming/1 1D/1_fibo_&_climbing_stairs.cpp
+++ 3 Arrays, Strings and Hashing/8 Recursion, Backtracking and DP/4 Dynamic Programming/1 1D/1_fibo_&_climbing_stairs.cpp
@@ -53,15 +53,16 @@ int fibonacci(int n)
 {
     if (n <= 1)
         return n;
-    int dp[2];
+    int dp[3];
     dp[0] = 0;
     dp[1] = 1;
     for (int i = 2; i <= n; i++)
     {
         int p1 = dp[1];
         int p2 = dp[0];
+        dp[2] = p1+p2;
         dp[0] = dp[1];
-        dp[1] = p1 + p2;
+        dp[1] = dp[2];
     }
     return dp[1];
 }
