51197c9fcb8a521abfa4102b4ab7229398373028
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-25 12:05:41 +0545

    add new problem

---
+++ 2 Mathematics/6 Bits Manipulation/8_check_even.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkOdd(int num)
+{
+    return num & 1; //  num % 2
+}
+// note while using comparator operator don't do num & 1 == 1 instead (num & 1) == 1
+
+int main()
+{
+
+    return 0;
+}
