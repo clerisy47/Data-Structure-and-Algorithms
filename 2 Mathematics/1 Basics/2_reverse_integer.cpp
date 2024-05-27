0267f2d17acf68c10f40688c37d0c89e9e0a7a9d
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-27 17:00:38 +0545

    add problem link

--- 2 Mathematics/1 Basics/2_reverse_integer.cpp
+++ 2 Mathematics/1 Basics/2_reverse_integer.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/reverse-integer/description/
+
 #include <bits/stdc++.h>
 
 int reverse(int x)
@@ -11,5 +13,20 @@ int reverse(int x)
     return rev;
 }
 
-// Time Complexity O(n) 
+// Time Complexity O(n)
 // Space Complexity O(1)
+
+// if reversing an integer may overflow then return 0
+
+int reverse(int x)
+{
+    int rev = 0;
+    while (x != 0)
+    {
+        if (rev > INT_MAX / 10 || rev < INT_MIN / 10)
+            return 0; // check 32 bit range if r is outside the range
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+    return rev;
+}
