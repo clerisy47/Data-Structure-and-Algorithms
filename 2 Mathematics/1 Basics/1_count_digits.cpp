45ab39a7d9b75206d1f9fa205aa3be7c378cae4b
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-27 17:00:36 +0545

    add problem link

--- 2 Mathematics/1 Basics/1_count_digits.cpp
+++ 2 Mathematics/1 Basics/1_count_digits.cpp
@@ -1,36 +1,36 @@
-#include<bits/stdc++.h>
+// https://www.geeksforgeeks.org/problems/count-digits5716/1
+
+#include <bits/stdc++.h>
 using namespace std;
 
 // Brute Force
-int countDigits(int num)
+int countDigits(int n)
 {
-    int count = 0, rem;
-    while (num != 0)
+    int count = 0, digit;
+    while (n != 0)
     {
-        rem = num % 10;
-        count += 1;
-        num /= 10;
+        digit = n % 10;
+        count++;
+        n /= 10;
     }
     return count;
 }
 // TC: O(log10(n))
 // SC: O(1)
 
-//Optimal assuming num is natural number
-int countDigits(int num)
+// Optimal assuming n is natural number
+int countDigits(int n)
 {
-    return floor(log10(num)) + 1;
+    return floor(log10(n)) + 1;
 }
 
 // TC: O(1)
 // SC: O(1)
 
+int main()
+{
 
-
-int main(){
-    
     return 0;
 }
-
 
 // floor(log10(x)) + 1  for best time and space complexity
