c22e44c5ade904ab271a1c2245f03190ee38291b
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-28 13:40:20 +0545

    modify loop condition

--- 2 Mathematics/1 Basics/2_reverse_integer.cpp
+++ 2 Mathematics/1 Basics/2_reverse_integer.cpp
@@ -5,10 +5,9 @@
 int reverse(int x)
 {
     int rev = 0;
-    while (x != 0)
+    for (int i = x; i != 0; i /= 10)
     {
-        rev = rev * 10 + x % 10;
-        x /= 10;
+        rev = rev * 10 + i % 10;
     }
     return rev;
 }
@@ -21,12 +20,12 @@ int reverse(int x)
 int reverse(int x)
 {
     int rev = 0;
-    while (x != 0)
+
+    for (int i = x; i != 0; i /= 10)
     {
         if (rev > INT_MAX / 10 || rev < INT_MIN / 10)
             return 0; // check 32 bit range if r is outside the range
-        rev = rev * 10 + x % 10;
-        x /= 10;
+        rev = rev * 10 + i % 10;
     }
     return rev;
 }
