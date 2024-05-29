d9b3a4b79e29281743d16862f0781adc8be2363f
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-05-29 16:24:02 +0545

    add function

--- 1 Notes and STL/2_arrays_and_vectors.cpp
+++ 1 Notes and STL/2_arrays_and_vectors.cpp
@@ -51,13 +51,16 @@ int main()
     v.pop_back(); // O(1)
     printVec(v);
 
-    vector<int> v_zeroes(10); 
+    v.erase(v.begin() + 2); // O(n)
+    printVec(v);
+
+    vector<int> v_zeroes(10);
     printVec(v_zeroes);
     vector<int> v_ones(10, 1);
     printVec(v_ones);
 
     // or use fill
-    fill(v_zeroes.begin(), v_zeroes.end(), 0); //int arr[n]; fill(arr, n, 0);
+    fill(v_zeroes.begin(), v_zeroes.end(), 0); // int arr[n]; fill(arr, n, 0);
 
     // Copying of vectors are shallow copy by default
     vector<int> v_copy = v;
