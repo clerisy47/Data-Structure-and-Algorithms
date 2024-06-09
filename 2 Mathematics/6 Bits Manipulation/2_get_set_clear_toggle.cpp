058e6e676e03e4efde449cb63197edacbc4dba39
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-09 19:28:16 +0545

    removed bugs

--- 2 Mathematics/6 Bits Manipulation/2_get_set_clear_toggle.cpp
+++ 2 Mathematics/6 Bits Manipulation/2_get_set_clear_toggle.cpp
@@ -1,20 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Brute force: Convert into binary and do manipulation
 
 // Optimal solution
-vector<int> bitManipulation(int num, int k){
-    int i = k-1; // Getting index. If question has 0 indexing then no need
-    // bool get = num&(1<<i)!=1;
-    bool get = 1&(num>>i);
-    int set = num|(1<<i);
-    int clear = num&~(1<<i);
-    int toggle = num^(1<<i);
-    return {get, set, clear, toggle};  // bool and int gets typecasted by itself. bool becomes 1 if non zero
+vector<int> bitManipulation(int num, int k)
+{
+    int i = k - 1; // 1 based indexing
+    bool get = num & (1 << i);
+    // bool get = 1 & (num >> i);
+    int set = num | (1 << i);
+    int clear = num & ~(1 << i);
+    int toggle = num ^ (1 << i);
+    return {get, set, clear, toggle}; // bool and int gets typecasted by itself. bool becomes 1 if non zero
 }
 
-int main(){
-    
+int main()
+{
+    int n;
+    cin >> n;
     return 0;
 }
