a9fa65dcfe509abb493d27bb67cf667d5606cb99
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-09 19:27:50 +0545

    add vector form

--- 2 Mathematics/6 Bits Manipulation/1_binary_decimal.cpp
+++ 2 Mathematics/6 Bits Manipulation/1_binary_decimal.cpp
@@ -2,21 +2,41 @@
 
 int binary_decimal(long n)
 {
-    int result = 0;
+    int res = 0;
     for (int i = 0; n != 0; i++)
     {
         int rem = n % 10;
-        result += rem * pow(2, i);
+        res += rem * pow(2, i);
         n /= 10;
     }
+    return res;
 }
 long decimal_binary(int n)
 {
-    long result = 0;
+    long res = 0;
     for (int i = 0; n != 0; i++)
     {
         int rem = n % 2;
-        result += rem * pow(10, i);
+        res += rem * pow(10, i);
         n /= 2;
     }
+    return res;
+}
+
+// in vector form
+
+vector<int> decimal_binary_v(int n)
+{
+    if (n == 0)
+    {
+        return {0};
+    }
+    vector<int> res;
+    for (; n != 0; n /= 2)
+    {
+        int rem = n % 2;
+        res.push_back(rem);
+    }
+    reverse(res.begin(), res.end());
+    return res;
 }
