6f04f6236340f30f789bd971b301d9bf17632069
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-10 19:09:29 +0545

    add stl popcount function

--- 2 Mathematics/6 Bits Manipulation/9_number_of_set_bits.cpp
+++ 2 Mathematics/6 Bits Manipulation/9_number_of_set_bits.cpp
@@ -1,17 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Optimal (for some cases)
 int countSetBits(int num)
 {
-    int count=0;
-    while(num>0){
+    int count = 0;
+    while (num > 0)
+    {
         // if(num%2==1){
         //     count++;
         // }
-        count+= num&1;
+        count += num & 1;
         // num/=2;
-        num=num>>1;
+        num = num >> 1;
     }
     return count;
 }
@@ -20,16 +21,25 @@ int countSetBits(int num)
 // Optimal (for most cases)
 int countSetBits(int num)
 {
-    int count=0;
-    while(num>0){
-        num = num & (num-1);
+    int count = 0;
+    while (num > 0)
+    {
+        num = num & (num - 1);
         count++;
     }
     return count;
 }
 // TC: O(n) where n = number of sets
 
-int main(){
-    
+// stl
+int countSetBits(int num)
+{
+    return __builtin_popcount(num);   // number of set bits in the number
+    return __builtin_popcountll(num); // number of set bits in the ll number
+}
+
+int main()
+{
+
     return 0;
 }
