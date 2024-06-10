35cc5d74f5aaa0b95bb3004a840cc7bb2d9d4459
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-10 19:08:56 +0545

    add stl methods

---
+++ 1 Standard Template Library/10_misc.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+
+
+    cout << __builtin_popcount(5) << endl; // number of set bits in the number
+    cout << __builtin_popcountll(1ll) << endl; // number of set bits in the ll number
+
+    cout << __gcd(5, 6);
+}
