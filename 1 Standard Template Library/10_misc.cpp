811dc6988f2d3ff76626b9c7475f7f111eb9a988
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-15 18:52:19 +0545

    update comments

--- 1 Standard Template Library/10_misc.cpp
+++ 1 Standard Template Library/10_misc.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
 
+    int n1 = 2, n2 = 3, n3 = 5, n4 = 9;
+    cout << min({n1, n2, n3, n4}) << endl;
+    cout << max({n1, n2, n3, n4}) << endl;
 
+    long long n5 = 7;
+    cout << __builtin_popcount(n3) << endl;   // number of set bits in the number
+    cout << __builtin_popcountll(n5) << endl; // number of set bits in the ll number
 
-    cout << __builtin_popcount(5) << endl; // number of set bits in the number
-    cout << __builtin_popcountll(1ll) << endl; // number of set bits in the ll number
-
-    cout << __gcd(5, 6);
+    cout << __gcd(n2, n4);
 }
