48173542b8d01385104f6b2e5386ae7931fde6f7
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-21 11:47:13 +0545

    add new problem

---
+++ 6 Mixed/900/25_bad_boy.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/1537/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define INFINITY LLONG_MAX >> 1
+#define int long long
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << 1 << " " << 1 << " " << x1 << " " << y1 << endl;
+    }
+    return 0;
+}
