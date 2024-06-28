cea229062f9228d7d07b73098da8de2aa9b50756
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-06-28 10:30:25 +0545

    add new problem

---
+++ 6 Mixed/800/57_Ichihime_and_Triangle.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/1337/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define INFINITY LLONG_MAX >> 1
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n1, n2, n3, n4;
+        cin >> n1 >> n2 >> n3 >> n4;
+        cout << n2 << " " << n3 << " " << n3 << endl;
+    }
+    return 0;
+}
