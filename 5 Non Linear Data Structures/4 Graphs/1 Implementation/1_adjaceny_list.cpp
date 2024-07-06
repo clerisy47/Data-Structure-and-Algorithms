a069bbdf0b1bfb7ade6a5cf72f417dc348b86834
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-06 18:53:25 +0545

    add function for adjacency list

--- 5 Non Linear Data Structures/4 Graphs/1 Implementation/1_adjaceny_list.cpp
+++ 5 Non Linear Data Structures/4 Graphs/1 Implementation/1_adjaceny_list.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+vector<vector<int>> inputGraphAdjacencyList(int n1, int n2)
 {
-    int n, m, v1, v2;
-    cin >> n >> m;  // number of vertices and nodes   // in tree, m = n-1
-    vector<vector<int>> graph(n+1); // 1 based indexing
-    for (int i = 1; i <= m; i++)
+    int v1, v2;
+    vector<vector<int>> graph(n1 + 1); // 1 based indexing
+    for (int i = 1; i <= n2; i++)
     {
         cin >> v1 >> v2;
-        graph[v1].push_back(v2); 
+        graph[v1].push_back(v2);
         graph[v2].push_back(v1); // if it is bidirectional
         // for weighted graph, push_back pairs instead of integer
     }
+}
+
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2; // number of vertices and nodes   // in tree, n2 = n1-1
+    inputGraphAdjacencyList(n1, n2);
     return 0;
 }
