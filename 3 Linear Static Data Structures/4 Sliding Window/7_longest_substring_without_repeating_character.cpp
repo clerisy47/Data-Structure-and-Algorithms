566e7926ad7b3182280e68f82e58492ba25bd1da
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-06 20:38:42 +0545

    rename

--- 3 Linear Static Data Structures/4 Sliding Window/7_longest_substring_without_repeating_character.cpp
+++ 3 Linear Static Data Structures/4 Sliding Window/7_longest_substring_without_repeating_character.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
 
 int lengthOfLongestSubstring(string s)
@@ -9,12 +10,12 @@ int lengthOfLongestSubstring(string s)
     map<char, int> hash;
     for (int i = 0, j = 0; j <= n - 1; j++)
     {
-        if (hash.find(s[j]) != hash.end()&&hash[s[j]]>=i) // &&hash[s[j]]>=i to ensure indexes before the window not to be considered
+        if (hash.find(s[j]) != hash.end() && hash[s[j]] >= i) // &&hash[s[j]]>=i to ensure indexes before the window not to be considered
         {
             i = hash[s[j]] + 1;
         }
-        hash[s[j]] = j; 
-        length = j - i + 1;  
+        hash[s[j]] = j;
+        length = j - i + 1;
         maxLength = max(maxLength, length);
     }
     return maxLength;
@@ -24,6 +25,8 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    string s1 = "abcabcbb";
+    cout << lengthOfLongestSubstring(s1) << endl;
 
     return 0;
 }
