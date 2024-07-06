d8bf8955d8eaf5e42af8ea5aa19ee46a71d944ae
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-06 18:53:28 +0545

    add function for adjacency list

--- 5 Non Linear Data Structures/4 Graphs/1 Implementation/2_adjacency_matrix.cpp
+++ 5 Non Linear Data Structures/4 Graphs/1 Implementation/2_adjacency_matrix.cpp
@@ -1,18 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+vector<vector<int>> inputGraphAdjacencyMatrix(int n1, int n2)
 {
-    int n, m, v1, v2;
-    cin >> n >> m;
-    vector<vector<int>> graph(n, vector<int>(n)); // 1 based indexing
-    for (int i = 0; i <= m-1; i++)
+    int v1, v2;
+    vector<vector<int>> graph(n1, vector<int>(n1)); // 1 based indexing
+    for (int i = 0; i <= n2 - 1; i++)
     // use 0 based indexing even if in question 1 based indexing is given
     {
         cin >> v1 >> v2;
-        graph[v1-1][v2-1] = 1; 
-        graph[v1-1][v2-1] = 1; // if it is bidirectional
+        graph[v1 - 1][v2 - 1] = 1;
+        graph[v1 - 1][v2 - 1] = 1; // if it is bidirectional
         // for weighted graph, 1 becomes wt
     }
+}
+
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
+    inputGraphAdjacencyMatrix(n1, n2);
     return 0;
 }
