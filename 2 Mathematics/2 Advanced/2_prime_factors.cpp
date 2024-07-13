bbd7793d626b03ea767ad99658d06fd384687e2f
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-13 22:46:19 +0545

    add better prime factorization method

--- 2 Mathematics/2 Advanced/2_prime_factors.cpp
+++ 2 Mathematics/2 Advanced/2_prime_factors.cpp
@@ -2,21 +2,39 @@
 
 // Optimized version in sieve folder
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void printVector(vector<int>& vec) {
-    for(const auto& val : vec) {
+void printVector(vector<int> &vec)
+{
+    for (const auto &val : vec)
+    {
         cout << val << ' ';
     }
     cout << endl;
 }
 
 // Brute
-bool isPrime(int n){
-    if(n<=1) return false;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0) return false;
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    if (n == 2)
+    {
+        return true;
+    }
+    if (n & 1 == 0)
+    {
+        return false;
+    }
+    for (int i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
     }
     return true;
 }
@@ -24,8 +42,10 @@ bool isPrime(int n){
 vector<int> primeFactors(int n)
 {
     vector<int> ans;
-    for(int i=2;i<=n;i++){
-        if(isPrime(i)&&n%i==0){
+    for (int i = 2; i <= n; i++)
+    {
+        if (isPrime(i) && n % i == 0)
+        {
             ans.push_back(i);
         }
     }
@@ -37,16 +57,17 @@ vector<int> primeFactors(int n)
 {
     vector<int> output;
     int i = 2;
-    for(int i=2; i*i<=n;i++)
+    for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
         {
             output.push_back(i);
-            while(n%i==0)
+            while (n % i == 0)
                 n /= i;
         }
     }
-    if(n!=1) output.push_back(n); // to insert factor that comes after i*i<=n
+    if (n != 1)
+        output.push_back(n); // to insert factor that comes after i*i<=n
     return output;
 }
 
@@ -55,14 +76,14 @@ vector<int> primeFactors(int n)
 
 // Optimized version in sieve folder
 
-
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    cin>>n;
-    vector<int>  output =primeFactors(n) ;
+    cin >> n;
+    vector<int> output = primeFactors(n);
     printVector(output);
-    
+
     return 0;
 }
