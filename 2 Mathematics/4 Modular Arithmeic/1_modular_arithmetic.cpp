a1f70948abab2ce92131e3d531e6985a67fbaf7a
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-18 22:26:24 +0545

    optimize power function

--- 2 Mathematics/4 Modular Arithmeic/1_modular_arithmetic.cpp
+++ 2 Mathematics/4 Modular Arithmeic/1_modular_arithmetic.cpp
@@ -1,61 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define mod 1000000007ll
+#define endl '\n'
+#define inf LLONG_MAX >> 1
 
-#define ll long long
-int MOD = 1e9+7;
-
-int mod_add(int x, int y)
+int modAdd(int x, int y)
 {
-    return (x % MOD + 0LL + y % MOD) % MOD;
+    return (x % mod + y % mod) % mod;
 }
 
-int mod_sub(int x, int y)
+int modSub(int x, int y)
 {
-    return (x % MOD - y % MOD + MOD) % MOD;
+    return (x % mod - y % mod + mod) % mod;
 }
 
-int mod_multiply(int x, int y)
+int modMultiply(int x, int y)
 {
-    return (x % MOD * 1LL * y % MOD) % MOD;
+    return (x % mod * y % mod) % mod;
 }
 
-// x <= 1e18 y <=1e9 MOD == 1e9+7
-int mod_power(int x, int y)
+// x <= 1e18 y <=1e9
+int modPower(int x, int y)
 {
     int ans = 1;
     while (y)
     {
         if (y & 1)
-            ans = mod_multiply(ans, x);
-        x = mod_multiply(x, x);
+        {
+            ans = ans * x % mod;
+        }
+        x = x * x % mod;
         y >>= 1;
     }
     return ans;
 }
 // TC: log(n)
 
-// x <= 1e18 y <=1e18 z == 1e9 + 7
-int mod_multiply_binary(ll x, ll y)
+// x <= 1e18 y <=1e18
+int modMultiply2(int x, int y)
 {
-    ll ans = 0;
+    int ans = 0;
     while (y)
     {
         if (y & 1)
-            ans = mod_add(ans, x);
-        x = mod_add(x, x);
-        y >>= 1;
+            ans = (ans + x) % mod;
+        x = (x + x) % mod;
+        y >>= 1ll;
     }
+    return ans;
 }
-int mod_power(ll x, ll y)
+
+int modPower2(int x, int y)
 {
-    ll ans = 1;
+    int ans = 1;
     while (y)
     {
         if (y & 1)
         {
-            ans = mod_multiply_binary(ans, x);
+            ans = modMultiply(ans, x);
         }
-        x = mod_multiply_binary(x, x);
+        x = modMultiply(x, x);
         y >>= 1;
     }
     return ans;
@@ -65,12 +70,12 @@ int mod_power(ll x, ll y)
 int mod_inverse(int x)
 {
     // modulus multiplicative inverse
-    return mod_power(x, MOD - 2);
+    return modPower(x, mod - 2);
 }
 
 int mod_divide(int x, int y)
 {
-    return mod_multiply(x, mod_inverse(y));
+    return modMultiply(x, mod_inverse(y));
 }
 
 int mod_factorial(int x)
@@ -78,31 +83,36 @@ int mod_factorial(int x)
     int ans = 1;
     for (int i = 2; i <= x; i++)
     {
-        ans = mod_multiply(ans, i);
+        ans = modMultiply(ans, i);
     }
     return ans;
 }
 
-int main()
+signed main()
 {
-    cout << "Testing mod_add:" << endl;
-    cout << "mod_add(1e9, 1e9)= " << mod_add(1e9, 1e9) << endl;
-    cout << "mod_add(12, 5)= " << mod_add(12, 5) << endl;
+    cout << "Testing modAdd:" << endl;
+    cout << "modAdd(1e9, 1e9)= " << modAdd(1e9, 1e9) << endl;
+    cout << "modAdd(12, 5)= " << modAdd(12, 5) << endl;
 
-    // Test mod_sub
-    cout << "\nTesting mod_sub:" << endl;
-    cout << "mod_sub(20, 10)= " << mod_sub(20, 10) << endl;
-    cout << "mod_sub(100, 19)= " << mod_sub(100, 19) << endl;
+    // Test modSub
+    cout << "\nTesting modSub:" << endl;
+    cout << "modSub(20, 10)= " << modSub(20, 10) << endl;
+    cout << "modSub(100, 19)= " << modSub(100, 19) << endl;
 
-    // Test mod_multiply
-    cout << "\nTesting mod_multiply:" << endl;
-    cout << "mod_multiply(10, 20)= " << mod_multiply(10, 20) << endl;
-    cout << "mod_multiply(10000, 100000)= " << mod_multiply(100000, 100000) << endl;
+    // Test modMultiply
+    cout << "\nTesting modMultiply:" << endl;
+    cout << "modMultiply(10, 20)= " << modMultiply(10, 20) << endl;
+    cout << "modMultiply(10000, 100000)= " << modMultiply(100000, 100000) << endl;
 
-    // Test mod_power
-    cout << "\nTesting mod_power:" << endl;
-    cout << "mod_power(2, 10) " << mod_power(2, 10) << endl;
-    cout << "mod_power(10, 1000000000) " << mod_power(10, 1000000000) << endl;
+    // Test modPower
+    cout << "\nTesting modPower:" << endl;
+    cout << "modPower(2, 10) " << modPower(2, 10) << endl;
+    cout << "modPower(10, 1000000000) " << modPower(10, 1000000000) << endl;
+
+    // Test modPower2
+    cout << "\nTesting modPower2:" << endl;
+    cout << "modPower(2, 10) " << modPower(2, 10) << endl;
+    cout << "modPower(10, 1000000000) " << modPower(10, 1000000000000) << endl;
 
     // Test mod_divide
     cout << "\nTesting mod_divide:" << endl;
