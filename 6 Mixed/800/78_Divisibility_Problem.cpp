117571201dd7cc3473640e86f51622275b2eeb75
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-20 09:43:34 +0545

    add new program

---
+++ 6 Mixed/800/78_Divisibility_Problem.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/problemset/problem/1328/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define INFINITY LLONG_MAX >> 1
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+        int r1 = n2 - n1 % n2;
+        if (r1 == n2)
+        {
+            r1 = 0;
+        }
+        cout << r1 << endl;
+    }
+    return 0;
+}
