c7ac38964db87f8afe7e3a9d997c4875e60adb60
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-29 20:46:37 +0545

    fix: 0 based indexing

--- 5 Non Linear Data Structures/4 Graphs/2 DFS and BFS/9_diameter_of_tree.cpp
+++ 5 Non Linear Data Structures/4 Graphs/2 DFS and BFS/9_diameter_of_tree.cpp
@@ -9,7 +9,7 @@ void dfs(int vertex, int parent, vector<vector<int>> &graph, vector<int> &depth)
     {
         if (child != parent)
         {
-            depth[child]=depth[vertex]+1;
+            depth[child] = depth[vertex] + 1;
             dfs(child, vertex, graph, depth);
         }
     }
@@ -21,19 +21,19 @@ int main()
     cin.tie(NULL);
     int n, m, v1, v2;
     cin >> n;
-    m=n-1;
-    vector<vector<int>> graph(n+1);
+    m = n - 1;
+    vector<vector<int>> graph(n + 1);
     for (int i = 0; i <= m - 1; i++)
     {
         cin >> v1 >> v2;
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
     }
-    vector<int> depth(n+1);
-    dfs(1, 0, graph, depth);
-    int max_vertex = max_element(depth.begin(), depth.end())-depth.begin();
+    vector<int> depth(n + 1);
+    dfs(0, -1, graph, depth);
+    int max_vertex = max_element(depth.begin(), depth.end()) - depth.begin();
     fill(depth.begin(), depth.end(), 0);
-    dfs(max_vertex, 0, graph, depth);
-    cout<< *max_element(depth.begin(), depth.end());
+    dfs(max_vertex, -1, graph, depth);
+    cout << *max_element(depth.begin(), depth.end());
     return 0;
 }
