31fa3982aa75fa79bd8017314fc7d2eddc746df9
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-08-03 15:21:17 +0545

    feat: add new problem

---
+++ 6 Mixed/1000/47_Lucky_Sum_of_Digits.cpp
@@ -0,0 +1,59 @@
+// https://codeforces.com/problemset/problem/109/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define debug(...) 47
+#else
+#include "debug.h"
+#endif
+#define int long long
+#define float long double
+#define mod 1000000007ll
+#define endl '\n'
+#define inf LLONG_MAX >> 1
+#define cinv(v, n) do { (v).resize(n); for (int i = 0; i < (n); ++i) cin >> (v)[i]; } while (0)
+#define coutv(v) do { for (const auto& elem : (v)) cout << elem << " "; cout << endl; } while (0)
+#define cin2d(v, rows, cols) do { (v).resize(rows, vector<int>(cols)); for (int i = 0; i < (rows); ++i) for (int j = 0; j < (cols); ++j) cin >> (v)[i][j]; } while (0)
+#define cin2d(v, rows, cols) do { (v).resize(rows, vector<int>(cols)); for (int i = 0; i < (rows); ++i) for (int j = 0; j < (cols); ++j) cin >> (v)[i][j]; } while (0)
+#define cout2d(v) do { for (const auto& row : (v)) { for (const auto& elem : row) cout << elem << " "; cout << endl; } } while (0)
+#define coutb(CONDITION) cout << (CONDITION ? "YES" : "NO") << endl;
+constexpr int pctr(int x) { return __builtin_popcountll(x); }
+
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<=1e6; i++){
+        if(n-4*i<0){
+            cout<<-1<<endl;
+            return;
+        }
+        if((n-4*i)%7==0){
+            int j = (n-4*i)/7;
+            for(int k=0; k<=i-1; k++){
+                cout<<4;
+            }
+            for(int k=0; k<=j-1; k++){
+                cout<<7;
+            }
+            cout<<endl;
+            return;
+        }
+    }
+    cout<<"error"<<endl;
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc=1;
+    // cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    return 0;
+}
