0c7536629ed0ad005b781efe97108cdd86b96d80
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-08-14 20:21:35 +0545

    feat: add new problem

---
+++ 6 Mixed/1000/70_Lovely_Palindromes.cpp
@@ -0,0 +1,46 @@
+// https://codeforces.com/problemset/problem/688/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define debug(...) 47
+#else
+#include "debug.h"
+#endif
+#define int long long
+#define float long double
+#define mod 1000000007ll
+#define endl '\n'
+#define inf LLONG_MAX >> 1
+#define all(x) (x).begin(), (x).end()
+#define cinv(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i]; } while (0)
+#define coutv(vec) do { for (const auto& elem : (vec)) cout << elem << " "; cout << endl; } while (0)
+#define cin2d(vec, rows, cols) do { (vec).resize(rows, vector<int>(cols)); for (int i = 0; i < (rows); ++i) for (int j = 0; j < (cols); ++j) cin >> (vec)[i][j]; } while (0)
+#define cinvp(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i].first >> (vec)[i].second; } while (0)
+#define coutvp(vec) do { for (const auto& p : (vec)) cout << p.first << " " << p.second << " "; cout << endl; } while (0)
+#define cout2d(vec) do { for (const auto& row : (vec)) { for (const auto& elem : row) cout << elem << " "; cout << endl; } } while (0)
+#define coutb(CONDITION) cout << (CONDITION ? "YES" : "NO") << endl;
+constexpr int pctr(int x) { return __builtin_popcountll(x); }
+
+
+
+inline void solve()
+{
+    string s;
+    cin>>s;
+    cout<<s;
+    reverse(s.begin(), s.end());
+    cout<<s<<endl;
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc=1;
+    // cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    return 0;
+}
