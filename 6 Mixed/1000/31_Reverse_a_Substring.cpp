4db97e0ffea05bc89895993b9f0f24f088bedcaa
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-09-24 16:48:58 +0545

    add new problem

---
+++ 6 Mixed/1000/31_Reverse_a_Substring.cpp
@@ -0,0 +1,60 @@
+// https://codeforces.com/problemset/problem/1155/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define debug(...) 47
+#else
+#include "debug.h"
+#endif
+#define int long long
+#define float long double
+#define mod 1000000007ll
+#define endl '\n'
+#define inf LLONG_MAX >> 1
+#define all(x) (x).begin(), (x).end()
+#define cinv(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i]; } while (0)
+#define coutv(vec) do { for (const auto& elem : (vec)) cout << elem << " "; cout << endl; } while (0)
+#define cin2d(vec, rows, cols) do { (vec).resize(rows, vector<int>(cols)); for (int i = 0; i < (rows); ++i) for (int j = 0; j < (cols); ++j) cin >> (vec)[i][j]; } while (0)
+#define cinvp(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i].first >> (vec)[i].second; } while (0)
+#define coutvp(vec) do { for (const auto& p : (vec)) cout << p.first << " " << p.second << " "; cout << endl; } while (0)
+#define cout2d(vec) do { for (const auto& row : (vec)) { for (const auto& elem : row) cout << elem << " "; cout << endl; } } while (0)
+#define coutb(CONDITION) cout << (CONDITION ? "YES" : "NO") << endl;
+#define __lcm(x, y) ((x) * (y) / __gcd((x), (y)))
+constexpr int pctr(int x) { return __builtin_popcountll(x); }
+
+
+
+inline void solve()
+{
+    int n;
+    string s;
+    cin>>n>>s;
+    string t=s;
+    sort(t.begin(), t.end());
+    if(s==t){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+        for(int i=0; i<=n-2; i++){
+            if(s[i]>s[i+1]){
+                cout<<i+1<<" "<<i+2<<endl;
+                return;
+            }
+        }
+    }
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc=1;
+    // cin >> tc;
+    
+    for(int i=0; i<=tc-1; i++){
+        solve();
+    }
+    return 0;
+}
