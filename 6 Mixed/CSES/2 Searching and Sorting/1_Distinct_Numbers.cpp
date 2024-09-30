e67e8973d482fcdd6bedccf3260576f704ce83d5
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-09-30 09:01:22 +0545

    feat: add new problem

---
+++ 6 Mixed/CSES/2 Searching and Sorting/1_Distinct_Numbers.cpp
@@ -0,0 +1,55 @@
+// https://cses.fi/problemset/task/1621
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define debug(...) 47
+#else
+#include "debug.h"
+#endif
+#define int long long
+#define float long double
+#define endl '\n'
+#define inf LLONG_MAX >> 1
+#define coutb(CONDITION) cout << (CONDITION ? "YES" : "NO") << endl;
+#define __lcm(n, n1) ((n) * (n1) / __gcd((n), (n1)))
+template <typename T>
+istream& operator>>(istream& in, vector<T>& vec) { for (auto& elem : vec) in >> elem; return in; }
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& vec) { for (const auto& elem : vec) out << elem << " "; return out; }
+template <typename T>
+istream& operator>>(istream& in, vector<vector<T>>& vec) { for (auto& row : vec) for (auto& elem : row) in >> elem; return in; }
+template <typename T>
+ostream& operator<<(ostream& out, const vector<vector<T>>& vec) { for (const auto& row : vec) { for (const auto& elem : row) out << elem << " "; } return out; }
+template <typename T1, typename T2>
+istream& operator>>(istream& in, vector<pair<T1, T2>>& vec) { for (auto& p : vec) in >> p.first >> p.second; return in; }
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const vector<pair<T1, T2>>& vec) { for (const auto& p : vec) out << p.first << " " << p.second << endl; return out; }
+constexpr int pctr(int n) { return __builtin_popcountll(n); }
+int mod = 1000000007ll;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    cin>>v;
+    set<int> s;
+    for(int i=0; i<=n-1; i++){
+        s.insert(v[i]);
+    }
+    cout<<s.size()<<endl;
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc=1;
+    // cin >> tc;
+    
+    for(int i=0; i<=tc-1; i++){
+        solve();
+    }
+    return 0;
+}
