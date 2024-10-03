8b01dbe3dd78bb993b3c38fde9d88478c536de78
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-10-03 14:28:29 +0545

    feat: add new program

---
+++ 6 Mixed/CSES/6 Mathematics/3_Exponentiation_II.cpp
@@ -0,0 +1,64 @@
+// https://cses.fi/problemset/task/1712
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define debug(...) 47
+#else
+#include "debug.h"
+#endif
+#define int long long
+#define float long double
+int mod = 1000000007;
+#define endl '\n'
+#define inf LLONG_MAX >> 1
+#define all(x) (x).begin(), (x).end()
+#define cinv(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i]; } while (0)
+#define coutv(vec) do { for (const auto& elem : (vec)) cout << elem << " "; cout << endl; } while (0)
+#define cin2d(vec, rows, cols) do { (vec).resize(rows, vector<int>(cols)); for (int i = 0; i < (rows); ++i) for (int j = 0; j < (cols); ++j) cin >> (vec)[i][j]; } while (0)
+#define cinvp(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i].first >> (vec)[i].second; } while (0)
+#define coutvp(vec) do { for (const auto& p : (vec)) cout << p.first << " " << p.second << " "; cout << endl; } while (0)
+#define cout2d(vec) do { for (const auto& row : (vec)) { for (const auto& elem : row) cout << elem << " "; cout << endl; } } while (0)
+#define coutb(CONDITION) cout << (CONDITION ? "YES" : "NO") << endl;
+#define __lcm(x, y) ((x) * (y) / __gcd((x), (y)))
+constexpr int pctr(int x) { return __builtin_popcountll(x); }
+
+int modMultiply2(int x, int y) {
+    return x*y %mod;
+}
+
+int modPower2(int x, int y) {
+    int r = 1;
+    for (; y > 0; x = modMultiply2(x, x), y >>= 1) {
+        if (y & 1) {
+            r = modMultiply2(r, x);
+        }
+    }
+    return r;
+}
+
+
+inline void solve()
+{
+    int n, n1, n2;
+    cin>>n>>n1>>n2;
+    mod--;
+    int r= modPower2(n1, n2);
+    mod++;
+    r= modPower2(n, r);
+    cout<<r<<endl;
+
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc=1;
+    cin >> tc;
+    
+    for(int i=0; i<=tc-1; i++){
+        solve();
+    }
+    return 0;
+}
