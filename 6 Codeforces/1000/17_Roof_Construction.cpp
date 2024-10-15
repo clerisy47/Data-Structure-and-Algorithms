d2fd6d95148e2ff39d560930e4c6d4decd9e30e7
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-10-15 10:51:04 +0545

    feat: add new problem

---
+++ 6 Codeforces/1000/17_Roof_Construction.cpp
@@ -0,0 +1,57 @@
+// https://codeforces.com/problemset/problem/1632/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define debug(...) 47
+#else
+#include "debug.h"
+#endif
+#define int long long
+#define float long double
+#define mod 1000000007ll
+#define endl '\n'
+#define inf LLONG_MAX >> 1
+#define all(x) (x).begin(), (x).end()
+#define cinv(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i]; } while (0)
+#define coutv(vec) do { for (const auto& elem : (vec)) cout << elem << " "; cout << endl; } while (0)
+#define cin2d(vec, rows, cols) do { (vec).resize(rows, vector<int>(cols)); for (int i = 0; i < (rows); ++i) for (int j = 0; j < (cols); ++j) cin >> (vec)[i][j]; } while (0)
+#define cinvp(vec, len) do { (vec).resize(len); for (int i = 0; i < (len); ++i) cin >> (vec)[i].first >> (vec)[i].second; } while (0)
+#define coutvp(vec) do { for (const auto& p : (vec)) cout << p.first << " " << p.second << " "; cout << endl; } while (0)
+#define cout2d(vec) do { for (const auto& row : (vec)) { for (const auto& elem : row) cout << elem << " "; cout << endl; } } while (0)
+#define coutb(CONDITION) cout << (CONDITION ? "YES" : "NO") << endl;
+#define __lcm(x, y) ((x) * (y) / __gcd((x), (y)))
+constexpr int pctr(int x) { return __builtin_popcountll(x); }
+#define ceil(x) (int) ceil(x)
+#define floor(x) (int) floor(x)
+
+
+
+inline void solve()
+{
+    int n;
+    cin>>n;
+    int t=1;
+    for(int i=n-1; i>1; i/=2, t*=2);
+    for(int i=1; i<=n-1; i++){
+        if(i==t){
+            cout<<0<<" "<<t<<" ";
+        }
+        else{
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    for(int i=0; i<=tc-1; i++){
+        solve();
+    }
+    return 0;
+}
